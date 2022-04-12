4fd198c69cec5dcc269670520f27678e0fec49ce
Nika Zelenska <zelenskaya.nika@gmail.com>
2022-04-12 18:40:00 +0300

    added pause of game/music and game_over conditions

--- js/controls.js
+++ js/controls.js
@@ -4,6 +4,9 @@ const life_html = document.querySelector('#life')
 const points_html = document.querySelector('#points')
 const shotSfx = document.querySelector('#shoot')
 const enemyShotSfx = document.querySelector('#enemy-shoot')
+const music_btn = document.querySelector('#btn-music')
+const pause_btn = document.querySelector('#btn-pause')
+const pause = document.querySelector('.pause')
 
 const game_audio = document.querySelector('#game-audio')
   game_audio.volume=0.3
@@ -18,6 +21,10 @@ const game_audio = document.querySelector('#game-audio')
 const speed = 5
 const coefficient = 0.5
 let count_down = 0
+let game = {
+  over:false,
+  active:true
+}
 
 enemyShotSfx.volume = 0.3
 shotSfx.volume = 0.3
@@ -149,8 +156,6 @@ class Player{
       y: canvas.height*0.86
     }
 
-    this.rotation =0
-
     const image = new Image()
       image.src = './img/ship2.png'
       image.onload=()=>{
@@ -166,17 +171,23 @@ class Player{
       y:0
     }
 
+    this.opacity=1
     this.life = 3
     this.points = 0
 
   }
 
   draw(){
+    c.save()
+    
+    c.globalAlpha = this.opacity
+
     if(this.image){
       c.drawImage(this.image, this.position.x,
         this.position.y, 
         this.width, this.height);
     }
+    c.restore()
   }
 
   update(){
@@ -307,161 +318,223 @@ const keys = {
 }
 
 
+function funExplosion({object,color}){
+  for(let indexP =0;indexP<10;indexP++){
+    particles.push(new Particle({position:{
+      x:object.position.x+object.width/2,
+      y:object.position.y+object.height/2,},
+      velosity:{
+        x:(Math.random()-0.5)*2,
+        y:(Math.random()-0.5)*2
+      },
+      radius:Math.random()*2,
+      color: color || 'yellow'
+  }))
+  }
+}
+
 // 
 function animate(){
-  requestAnimationFrame(animate)
-  
-  c.clearRect(0,0,canvas.width, canvas.height)
-  life.innerHTML = ''
-  life.innerHTML = player.life
+  if(game.active){
+    requestAnimationFrame(animate)
+    
+    c.clearRect(0,0,canvas.width, canvas.height)
+    life.innerHTML = ''
+    life.innerHTML = player.life
 
-  points.innerHTML = ''
-  points.innerHTML = player.points
+    points.innerHTML = ''
+    points.innerHTML = player.points
 
-  player.update()
+    player.update()
 
-  particles.forEach((particle, pIndex) =>{
-    if(particle.opacity<=0.01){
-      setTimeout(()=>{
-        particles.splice(pIndex,1)
-      },0)
-    }else(
-      particle.update()
-    )
-  })
+    particles.forEach((particle, pIndex) =>{
+      if(particle.opacity<=0.01){
+        setTimeout(()=>{
+          particles.splice(pIndex,1)
+        },0)
+      }else(
+        particle.update()
+      )
+    })
 
 
-  // console.log(`length of shots array: ${shots.length}`)
+    // console.log(`length of shots array: ${shots.length}`)
 
-  // checking enemy shots going out of game space
-  enemyShots.forEach((enemyShot, esIndex) =>{
-    if(enemyShot.position.y+enemyShot.height >= canvas.height){
-      setTimeout(()=>{
-        enemyShots.splice(esIndex,1)
-
-      },0)
-    }else{
-      enemyShot.update()
-    }
-  })
-
-  // cheking player shots going out of canvas space
-  shots.forEach((shot,index) => {
-    if ((shot.position.y+shot.radius)<=0){
-      setTimeout(()=>{
-        shots.splice(index,1)
-      }, 0)
-    }else{
-      shot.update()
-    }
-  })
-  count_down=0
-
-  // 
-  if(inv_group.length<=0){
-    inv_group.push(new GroupInvader())
-  }else{
-    inv_group.forEach((group,gridIndex)=>{
-      // spawn shots
-      if(frames%200===0 && group.invaders.length>0){
-        randomEI = Math.floor(Math.random()*group.invaders.length)
-        group.invaders[randomEI].shoot(enemyShots)
-        enemyShotSfx.play()
+    // checking enemy shots going out of game space
+    enemyShots.forEach((enemyShot, esIndex) =>{
+      if(enemyShot.position.y+enemyShot.height >= canvas.height){
+        setTimeout(()=>{
+          enemyShots.splice(esIndex,1)
+        },0)
+      }else{
+        enemyShot.update()
       }
-      
-      group.update()
-      group.invaders.forEach((invader,i)=>{
-        invader.update(group.velosity.x,group.velosity.y)
-        
-        shots.forEach((shot,j)=>{
-        
-          // some logic for player shot if it reaches an enemy
-          if((shot.position.y-shot.radius<=invader.position.y+invader.height)&&
-          (shot.position.x+shot.radius>=invader.position.x)&&
-          (shot.position.x-shot.radius<=invader.position.x+invader.width)&&
-          (shot.position.y+shot.radius>=invader.position.y)){
+
+      // enemy hits player
+      if(enemyShot.position.y+enemyShot.height >= player.position.y &&
+        enemyShot.position.x+enemyShot.width >= player.position.x &&
+        enemyShot.position.x<=player.position.x+player.width){
+          setTimeout(()=>{
+                  // pop the enemy shot from array
+                  enemyShots.splice(esIndex,1)
+
+          },0)
+
+          // EXPLOSIONS EVERYWHERE
+          funExplosion({
+            object:player,
+            color:'#48a0dc'
+          })
+
+          player.life-=1
+
+          if(player.life===0){
+            player.opacity =0
+            game.over=true
             
             setTimeout(()=>{
-              const invaderFound = group.invaders.find(invader2=>invader2===invader)
-              const shotFound = shots.find(shot2=>shot2===shot)
-  
-              // remove enemy and shot or "ENEMY GOT HITED BY SHOT"
-              if(invaderFound && shotFound){
-                // debugger;
-
-                // EXPLOSIONS EVERYWHERE
-                for(let indexP =0;indexP<10;indexP++){
-                  particles.push(new Particle({position:{
-                    x:invader.position.x+invader.width/2,
-                    y:invader.position.y+invader.height/2,},
-                    velosity:{
-                      x:(Math.random()-0.5)*2,
-                      y:(Math.random()-0.5)*2
-                    },
-                    radius:Math.random()*2,
-                    color:'yellow'
-                }))
-                }
-  
-                // pop the invader and shot from their arrays
-                group.invaders.splice(i,1)
-                shots.splice(j,1)
-                player.points+=50
-  
-                if(group.invaders.length >0){
-                  const firstInvader = group.invaders[0]
-                  const lastInvader = group.invaders[group.invaders.length-1]
-                  // console.log(`first:${firstInvader} and last ${lastInvader}`)
-                  group.width = lastInvader.position.x-firstInvader.position.x+lastInvader.width
-                  group.position.x=firstInvader.position.x
-                }else {
-                  inv_group.splice(gridIndex,1)
-                }
-              }
-            },0)
-  
+              game.active=false
+            },200)
           }
+
+          
+        }
+    })
+
+    // cheking player shots going out of canvas space
+    shots.forEach((shot,index) => {
+      if ((shot.position.y+shot.radius)<=0){
+        setTimeout(()=>{
+          shots.splice(index,1)
+        }, 0)
+      }else{
+        shot.update()
+      }
+    })
+    count_down=0
+
+    // 
+    if(inv_group.length<=0){
+      inv_group.push(new GroupInvader())
+    }else{
+      inv_group.forEach((group,gridIndex)=>{
+        // spawn shots
+        if(frames%200===0 && group.invaders.length>0){
+          randomEI = Math.floor(Math.random()*group.invaders.length)
+          group.invaders[randomEI].shoot(enemyShots)
+          enemyShotSfx.play()
+        }
+
+        group.update()
+        group.invaders.forEach((invader,i)=>{
+          invader.update(group.velosity.x,group.velosity.y)
+
+          shots.forEach((shot,j)=>{
+          
+            // some logic for player shot if it reaches an enemy
+            if((shot.position.y-shot.radius<=invader.position.y+invader.height)&&
+            (shot.position.x+shot.radius>=invader.position.x)&&
+            (shot.position.x-shot.radius<=invader.position.x+invader.width)&&
+            (shot.position.y+shot.radius>=invader.position.y)){
+
+              setTimeout(()=>{
+                const invaderFound = group.invaders.find(invader2=>invader2===invader)
+                const shotFound = shots.find(shot2=>shot2===shot)
+              
+                // remove enemy and shot or "ENEMY GOT HITED BY SHOT"
+                if(invaderFound && shotFound){
+                  // debugger;
+
+                  // EXPLOSIONS EVERYWHERE
+                  funExplosion({
+                    object:invader
+                  })
+                
+                  // pop the invader and shot from their arrays
+                  group.invaders.splice(i,1)
+                  shots.splice(j,1)
+                  player.points+=50
+                
+                  if(group.invaders.length >0){
+                    const firstInvader = group.invaders[0]
+                    const lastInvader = group.invaders[group.invaders.length-1]
+                    // console.log(`first:${firstInvader} and last ${lastInvader}`)
+                    group.width = lastInvader.position.x-firstInvader.position.x+lastInvader.width
+                    group.position.x=firstInvader.position.x
+                  }else {
+                    inv_group.splice(gridIndex,1)
+                  }
+                }
+              },0)
+            
+            }
+          })
         })
       })
-    })
-  }
+    }
 
   
 
 
-  // move player on game space
-  if(keys.a.pressed && player.position.x >=0){
-    player.velosity.x = -speed
-    player.rotation = -0.15
-  }else if (keys.d.pressed && (player.position.x+player.width) < canvas.width) {
-    player.velosity.x = speed
-    player.rotation = 0.15
-  }else if(keys.s.pressed && (player.position.y+player.height+15)<canvas.height){
-    player.velosity.y = speed
-  }else if(keys.w.pressed && player.position.y>=0){
-    player.velosity.y = -speed
-  }
-  else{
-    player.velosity.x=0
-    player.velosity.y=0
-    player.rotation = 0
-  }
+    // move player on game space
+    if(keys.a.pressed && player.position.x >=0){
+      player.velosity.x = -speed
+      player.rotation = -0.15
+    }else if (keys.d.pressed && (player.position.x+player.width) < canvas.width) {
+      player.velosity.x = speed
+      player.rotation = 0.15
+    }else if(keys.s.pressed && (player.position.y+player.height+15)<canvas.height){
+      player.velosity.y = speed
+    }else if(keys.w.pressed && player.position.y>=0){
+      player.velosity.y = -speed
+    }
+    else{
+      player.velosity.x=0
+      player.velosity.y=0
+      player.rotation = 0
+    }
 
-  // spawn enemy
-  // if(frames%randomInterval === 0){
-    
-    
-  //   randomInterval = Math.floor((Math.random()*1000)+500)
-  // }
-  frames++
+    frames++
+  }
 }
 
 animate()
+
+pause_btn.addEventListener('click',()=>{
+  if(game.active){
+    pause_btn.classList.add('clicked')
+    pause.classList.add("active")
+    game.active = false
+  }else{
+    pause_btn.classList.remove("clicked")
+    pause.classList.remove("active")
+    game.active=true
+    animate()
+  }
+
+  console.log(game.active)
+})
+
+let game_audio_paused = false
+
+music_btn.addEventListener('click',()=>{
+  if(game_audio_paused){
+    music_btn.classList.remove("clicked")
+    game_audio.play()
+    game_audio_paused = false
+  }else{
+    music_btn.classList.add('clicked')
+    game_audio.pause()
+    game_audio.currentTime=0
+    game_audio_paused = true
+  }
+})
 
 /** 
  * Event Listners part so your spaceship could move
 */
 addEventListener('keydown', ({key}) =>{
+  if(game.over) return
   switch(key){
     case 'a':
       keys.a.pressed=true
